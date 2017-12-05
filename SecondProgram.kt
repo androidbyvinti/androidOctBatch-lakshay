c30253ca1f42cba83cd29e6431b919db6bdfe4f3
androidbyvinti <32050504+androidbyvinti@users.noreply.github.com>
2017-12-05 17:17:26 +0530

    Add files via upload

---
+++ SecondProgram.kt
@@ -0,0 +1,16 @@
+
+//method --> main method -->
+
+//public static void main(String args[])
+
+fun main(i: Array<String>) {
+
+    var value1 = 10
+    var value2 = 20
+
+    println("Value1 = $value1")// String interpolation
+    println("Value2 = $value2")
+
+    println(value1 + value2)
+
+}
